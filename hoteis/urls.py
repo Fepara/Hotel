2fe2665d7da7531c12bc118e9b246fdb7cc4a928
felipe <felipepara25@gmail.com>
2024-11-05 07:27:31 -0300

    .

--- hoteis/urls.py
+++ hoteis/urls.py
@@ -1,4 +1,3 @@
-# hoteis/urls.py
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
--- hotel_Eco30/settings.py
+++ hotel_Eco30/settings.py
@@ -127,3 +127,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
+LANGUAGE_CODE = 'pt-br'
+
